dfda5861cad230812514cb461fe682cb87476aab
DavidVettuchirayil <DavidVettuchirayil@gmail.com>
2023-04-08 21:22:38 -0400

    A lot

--- Assignment03/javascript.js
+++ Assignment03/javascript.js
@@ -2,8 +2,12 @@ var appState = {
     activeView: "",
     username: "",
     quizOption: "",
+    quizTitle: "",
     questionType: "",
-    quizData: {}
+    questionNum: "",
+    quizData: {},
+    quizScore: 0,
+    encouragingMessage: ""
 }
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -15,6 +19,14 @@ document.addEventListener("DOMContentLoaded", function() {
                 appState.username = document.getElementById("username").value;
             }
             appState.quizOption = document.querySelector("input[name='quizOption']:checked").value;
+            if(appState.quizOption == "quiz1")
+            {
+                appState.quizTitle = "Placeholder1"
+            }
+            else
+            {
+                appState.quizTitle = "Placeholder2"
+            }
         }
         renderQuestion(1);
         return false;
@@ -41,6 +53,40 @@ async function getQuizData(questionNum)
 async function renderQuestion(questionNum)
 {
     appState.quizData = await getQuizData(questionNum);
+    appState.questionNum++;
     renderView("#quizView", "#activeView", appState);
     renderView("#" + appState.quizData.questionType, "#questionView", appState.quizData);
+}
+
+const checkAnswer = function(answer)
+{
+    if(answer == appState.quizData.answer)
+    {
+        appState.quizScore++;
+        correctAnswer();
+    }
+    else
+    {
+        incorrectAnswer();
+    }
+}
+
+const correctAnswer = function()
+{
+    var encouragingMessage = ["Wow!", "Good Job!", "You did it!", "Brilliant!", "Excellent!", "Nice Work!"];
+    var randomNum = Math.floor(Math.random() * encouragingMessage.length);
+    appState.encouragingMessage = encouragingMessage[randomNum];
+    renderView("#correctAnswer", "#questionView", appState);
+    setTimeout(() => {nextQuestion()}, 1000);
+}
+
+const incorrectAnswer = function()
+{
+    renderView("#incorrectAnswer", "#questionView", appState.quizData);
+}
+
+const nextQuestion = function()
+{
+    appState.questionNum++;
+    renderQuestion(appState.questionNum);
 }
